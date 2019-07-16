4c5bd3a34e611f3d01cb852b15ad03f69c34aecc
ezio <iixelite@gmail.com>
2019-07-16 13:19:06 +0200

    Added comments

--- Ezio/src/serverBranchesHandler.c
+++ Ezio/src/serverBranchesHandler.c
@@ -34,11 +34,14 @@ int main(int argc, char **argv)
 
     struct branch_handler_communication *branches_info;
 
+    //initializing cache address
     if((id_cache =  shmget(IPC_CACHE_KEY, CACHE_BYTES, IPC_CREAT|IPC_EXCL|0666)) == -1){
         perror("Error in shmget (cache): ");
         exit(-1);
     }
 
+    //initializing address of the struct that will be used by all the server branches
+    //to acquire handler creator informations
     if((id_info =  shmget(IPC_HNDLR_INFO_KEY, sizeof(struct handler_info), IPC_CREAT|IPC_EXCL|0666)) == -1){
         perror("Error in shmget (handler info): ");
         exit(-1);
@@ -49,16 +52,23 @@ int main(int argc, char **argv)
         exit(-1);
     }
 
+    //initializing semaphore which will be used by all server branches to
+    //acquire the listening socket and serve clients
     if(sem_init(&sem_tolistenfd, 1, 1) == -1){
         perror("Error in sem_init (sem_tolistenfd): ");
         exit(-1);
     }
 
+    //initializing the semaphore that the branches handler will post 2 times
+    //(setting its value to zero), after when he decide to merge 2 branches.
+    //Each branch involved in this procedure will post this semaphore when
+    //the transmisison of the clients is completed
     if(sem_init(&sem_trasfclients, 1, 2) == -1){
         perror("Error in sem_init (sem_trasfclients): ");
         exit(-1);
     }
 
+    //initializing listening socket
     memset(&address, 0, sizeof(address));
     address.sin_addr.s_addr = htonl(SERVER_ADDR);
     address.sin_port = htons(SERVER_PORT);
@@ -81,17 +91,22 @@ int main(int argc, char **argv)
 
     my_pid = getpid();
 
-    //inserting handler information into the shared struct
+    //inserting handler information into the shared structure
     info->pid = my_pid;
     info->listen_fd = listen_fd;
     info->sem_tolistenfd = &sem_tolistenfd;
     info->sem_transfclients = &sem_trasfclients;
 
+    //when the number of the active clients of each branch get to the 10%, 50% and 80%
+    //of MAX_CLI_PER_SB, it signals the creator with SIGUSR1.
+    //The branches handler will decide whether merge or create branches.
     if(signal(SIGUSR1, clients_has_changed) == SIG_ERR){
         perror("Error in signal: ");
         exit(-1);
     }
 
+    //initializing the memory populated with branch_handler_communication structures
+    //that the handler creator will use to send information to a specific server branch
     if((branches_info = mmap(NULL, MAX_NUM_SB*sizeof(struct branch_handler_communication),
                         PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, 0, 0)) == (void *)-1){
         perror("Error in mmap: ");
