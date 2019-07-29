8bd6f5b057342f17b38666b9d4750542707e87cb
ezio <iixelite@gmail.com>
2019-07-29 09:30:53 +0200

    Statuts: loop detector inserted, loop not found

--- Ezio/headers/checkClientPercentage.h
+++ Ezio/headers/checkClientPercentage.h
@@ -2,6 +2,8 @@
 // Created by ezio on 18/07/19.
 //
 
+#include <sys/resource.h>
+
 //function that checks the increasing/decreasing client connections
 void checkClientPercentage()
 {
@@ -53,6 +55,8 @@ void checkClientPercentage()
         return;
     }
 
+    int priority = 0;
+
     //every time that OLD PERF is different than NEW PERC,
     //send a signal to the server branches handler
     if(old_perc != new_perc){
@@ -62,6 +66,29 @@ void checkClientPercentage()
         if(kill(handler_info->pid, SIGUSR1) == -1){
             perror("Error in sending SIGUSR1 to handler: ");
             return;
+        }
+
+        //swhitching priority
+        switch((int)new_perc){
+            case 1:
+                priority = 0;
+                break;
+            case 2:
+                priority = -5;
+                break;
+            case 3:
+                priority = -10;
+                break;
+            case 4:
+                priority = -15;
+                break;
+            case 5:
+                priority = -20;
+                break;
+        }
+
+        if(setpriority(PRIO_PROCESS, getpid(), priority) == -1){
+            perror("Error in setpriority");
         }
     }
 
