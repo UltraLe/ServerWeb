d279481a08899922fadf5f7a8b509abc496395a8
ezio <iixelite@gmail.com>
2019-08-28 17:29:37 +0200

    Cache implementation

---
+++ Ezio/headers/cacheConst.h
@@ -0,0 +1,31 @@
+//
+// Created by ezio on 28/08/19.
+//
+
+#define MAX_IMAGE_DIM 400000                //Max dimension of an image in bytes
+
+#define MAX_HASH_KEYS 500                   //max keys used in has function
+
+#define MAX_IMAGE_NUM_PER_KEY 5            //the dimension of the array containing all the
+                                            //pictures having the same hash function key
+
+#define MAX_IMAGENAME_DIM 20                //max length of a image's name
+
+struct image{
+
+    char name[MAX_IMAGENAME_DIM];          //image name
+    char quality;                           //the double q (for example 0.8) has to be converted
+                                            //in an integer that goes from 0 to 9
+    unsigned short width;
+    unsigned short height;
+    char isPng;
+    char imageBytes[MAX_IMAGE_DIM];
+
+    unsigned int counter;                   //number of times that the image is accessed
+};
+
+struct hash_element{
+
+    unsigned short key;
+    struct image conflictingImages[MAX_IMAGE_NUM_PER_KEY];
+};
